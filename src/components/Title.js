da102824a73204370a9b99827d92d08dea6a479c
Agnieszka Korczak <korczak.a.m@gmail.com>
2019-02-12 14:16:22 +0100

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import style from './Title.css';
 
-const Title = props => <div><h1>{props.title}</h1> <h2>A list of the tasks: {props.taskslist}</h2></div>
+const Title = props => <div><h1 className={style.heading}>{props.title}</h1> <h2 className={style.list}>{props.taskslist}</h2> <h3 className={style.task}>{props.tasks} task(s)</h3></div>
 
 export default Title;
